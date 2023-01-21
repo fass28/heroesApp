b970245829d0f5e70867901ee862e78455731bb9
Alex Saavedra <fass28@icloud.com>
2023-01-21 16:28:52 -0500

    using hashrouter instead of browser mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <BrowserRouter basename={'/heroesApp'}>
+      <HashRouter>
         <HeroesApp />
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>,
 )
